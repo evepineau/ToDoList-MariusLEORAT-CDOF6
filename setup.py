a26c9285e2b783ff751f924dba623ecc014f157d
mariusokoto <130173829+mariusokoto@users.noreply.github.com>
2025-01-13 16:00:46 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from distutils.core import setup
-setup(
-name='ToDoList-MariusLEORAT-CDOF6',
-version='1.0',
-author='mariusokoto',
-license='MIT',
-long_description=open('README.md').read(),
-)
